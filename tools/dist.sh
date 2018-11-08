bcca1e2934cf1e0e405a41619f66b2ab9ad01b3d
Stanislav Bohm <spirali@kreatrix.org>
2018-11-08 11:35:53 +0100

    Tarball creating script

---
+++ tools/dist.sh
@@ -0,0 +1,15 @@
+set -e
+cd `dirname $0`/..
+
+cd docs
+sh build.sh
+cd ..
+
+TMP_TAR=/tmp/viadat.build.tar
+
+git archive master --prefix=viadat/ > $TMP_TAR
+cd ..
+tar --append --file=$TMP_TAR viadat/docs/arch.html viadat/docs/install.html viadat/docs/userguide.html
+cd viadat
+xz -c $TMP_TAR >viadat-0.1.tar.xz
+rm $TMP_TAR
