507e924d3170af709907a5c101bbb744f6600963
Josef Toman <josef@toman.email>
2019-05-23 17:59:38 +0200

    Better tools to udpate module repositories

--- hooks/SYMLINK_HOOKS.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-HOOKS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-for filename in "${HOOKS_DIR}/*"; do
-    cp --symbolic-link $filename "${HOOKS_DIR}/../.git/hooks"
-done
